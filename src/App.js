a05637e756ee4a7e7e73ec9969529591ff9e6bda
UTKARSHMENDHE <git@stackblitz.com>
2024-12-20 05:22:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import Utkarsh from './Utkarsh.js';
+import Yash from './Yash.js';
+
+export default function App() {
+  return (
+    <div>
+  <h1>Hello StackBlitz!</h1>
+    <Utkarsh></Utkarsh> 
+    <Yash></Yash>
+    </div>
+  );
+}
---
+++ src/Yash.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Yash(){
+  return (<div >
+    <div> Hi its Yash component </div>
+    <div> bye </div>
+</div>);
+}
+
+export default Yash;
